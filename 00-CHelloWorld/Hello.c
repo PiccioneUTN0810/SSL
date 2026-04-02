1f72a20cd02cc140257db9fd732df0d629529e83
PiccioneUTN0810 <ipiccione@frba.utn.edu.ar>
2026-04-02 16:18:49 -0300

    Update Hello.c

--- 00-CHelloWorld/Hello.c
+++ 00-CHelloWorld/Hello.c
@@ -1,11 +1,1 @@
-/* Qué: Trabajo00
- * Descripcion: Muestra por pantalla "Hello World"
- * Quiénes: Ignacio Piccione
- * Cuando: 20250413
- */
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
